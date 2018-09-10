3e8a648aac80fe48da6aac969aa42c11c49d0671
igor-dyrov <bazbazbaz.id@gmail.com>
2018-09-11 00:28:59 +0300

    add es6 string and history

--- index.js
+++ index.js
@@ -4,46 +4,39 @@ const app = express();
 app.get('/test-:id', function(req, res) {
     const options = {
         root: __dirname + '/public/',
-        // dotfiles: 'deny',
-        // headers: {
-        //     'x-timestamp': Date.now(),
-        //     'x-sent': true
-        // }
     };
 
-    res.sendFile('index.html', options, function(err) {
+    res.sendFile('index.html', options, err => {
         if (err){
             console.log("error : " + err);
         }
     });
-    // const text = req.params.id; //для шаблонизатора
 });
 
-app.get('/public/:filename', function(req,res) {
+app.get('/public/:filename', (req,res) => {
     const filename = req.params.filename;
     console.log("download file " + filename);
-    res.sendFile(filename, {root: __dirname + '/public/'}, function(err) {
+    res.sendFile(filename, {root: __dirname + '/public/'}, err => {
         if (err){
             console.log("error : " + err);
         }
     });
 });
 
-app.get('/templates/:filename', function (req,res){
+app.get('/templates/:filename',(req,res) => {
    const filename = req.params.filename;
    console.log("download file " + filename);
-    res.sendFile(filename, {root: __dirname + '/templates/'}, function(err) {
+    res.sendFile(filename, {root: __dirname + '/templates/'}, err => {
         if (err){
             console.log("In url: "+req.url+" error: " + err);
         }
     });
 });
 
-app.get('/static/*/:filename', function(req, res) {
+app.get('/static/*/:filename', (req, res) => {
     const filename = req.params.filename;
     let dir = __dirname + '/static/';
     urldirs = req.url.split('/');
-    //console.log(urldirs);
     switch (urldirs[2]) {
         case 'css':
             dir = dir + 'css';
@@ -52,7 +45,7 @@ app.get('/static/*/:filename', function(req, res) {
             dir = dir +'img';
             break;
     }
-    res.sendFile(filename, {root: dir}, function(err) {
+    res.sendFile(filename, {root: dir}, err => {
         if (err){
             console.log("In url: "+req.url+" error: " + err);
         }
--- public/test.js
+++ public/test.js
@@ -1,7 +1,26 @@
-const item = window.document.getElementById('test');
-//console.log("item " + item);
-url = window.location.toString();
-//console.log("url =" +url);
+'use strict';
 
-item.innerText = url.substring(url.lastIndexOf('-')+1, url.length);
+const incr = window.document.getElementById('incr');
+const item = window.document.getElementById('test');
+const decr = window.document.getElementById('decr');
+const warning = window.document.getElementById('warning');
+
+let url = window.location.toString();
+
+item.innerText = url.substring(url.lastIndexOf('-') + 1, url.length);
+
+decr.onclick = function() {
+	if (+item.innerText === 0) {
+        warning.innerHTML = '<p>Can`t make less than 0</p>';
+		return;
+	} 
+	item.innerText = item.innerText - 1;
+	window.history.pushState({id: item.innerText}, '', `test-${item.innerText}`);
+}
+
+incr.onclick = function() {
+	item.innerText = +item.innerText + 1;	
+	window.history.pushState({id: item.innerText}, '', `test-${item.innerText}`);
+    warning.innerHTML = '';
+}
 
