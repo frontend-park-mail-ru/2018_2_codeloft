86e1647af614a9879be15196734b70f9af017ddd
arthurunique24 <arthurunique24@gmail.com>
2018-09-13 00:53:13 +0300

    MainComponent and Input

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// import MainComponent from 'components/MainComponent/MainComponent.js';
+// import Input from './components/Input/Input.js';
+
+const MainDiv = document.getElementById('main');
+
+// const myInput = Input('text', 'userName', ['someClass'], 'Только попробуй написать');
+
+console.log(MainDiv);
+// console.log(myInput);
+
+// MainComponent.appendChild(myInput);
