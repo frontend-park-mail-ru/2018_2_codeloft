b3a4bacc72ff389a4ec3f05703cb103cbb641f94
arthurunique24 <arthurunique24@gmail.com>
2018-10-01 01:56:52 +0300

    Fix eslint

--- src/modules/Router/Router.js
+++ src/modules/Router/Router.js
@@ -9,26 +9,26 @@ class Router {
 		this.routesMap = {}
 		this.currentView = null
 	}
-    /**
+	/**
      * Инициализация работы приложения
      * Метод получает начальный url и отрисовывает нужную часть сайта
      * Вызывается при старте приложения
      */
-    start() {
-        window.onpopstate = (event => {
-            this._onRoute(window.location.pathname);
-        });
+	start() {
+		window.onpopstate = (event => {
+			this._onRoute(window.location.pathname)
+		})
 
 		this._onRoute(window.location.pathname)
 	}
 
-    /**
+	/**
      * внутренняя реализация отрисовки и перехода по урлам
      * @param pathname - урл, по которому и будет происходить изменения
      * @private
      */
-    _onRoute(pathname) {
-        const view = this.routesMap[pathname];
+	_onRoute(pathname) {
+		const view = this.routesMap[pathname]
 
 		if (!view) {
 			return
@@ -41,26 +41,26 @@ class Router {
 		this.currentView.show()
 	}
 
-    /**
+	/**
      * Добавление зависимости вьюшки от урла
      * @param path - урл(текст)
      * @param view - объект вьюшки
      * @return {Router} - возваращается сам объект
      * класса Router для красивого многострочного добавления
      */
-    add(path, view) {
-        this.routesMap[path] = view;
-        return this;
-    }
+	add(path, view) {
+		this.routesMap[path] = view
+		return this
+	}
 
-    /**
+	/**
      * Публичный метод для перехода по урлу и отрисовки нужной части страницы
      * @param path - сам урл, по которому необходим переход
      */
-    go(path) {
-        history.pushState({url: path}, '', path);
-        this._onRoute(path);
-    }
+	go(path) {
+		history.pushState({url: path}, '', path)
+		this._onRoute(path)
+	}
 }
 
 const router = new Router()
