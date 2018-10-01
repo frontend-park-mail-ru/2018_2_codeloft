a282acb4c4574ab686d3af743673dafd3f805576
arthurunique24 <arthurunique24@gmail.com>
2018-10-01 18:30:50 +0300

    Fix

--- src/components/ScoreTable/ScoreTable.js
+++ src/components/ScoreTable/ScoreTable.js
@@ -1,33 +1,74 @@
 'use strict';
 
 import MainComponent from '../MainComponent/MainComponent.js';
+import Transport from "../../modules/Transport/Transport.js";
 
 export default class ScoreTable extends MainComponent {
+	constructor(){
+		super();
+        this.usersFromBack = null;
+    }
 	compile(data) {
-		Handlebars.registerHelper('getRows', function () {
-			const user = {
-				name: 'Edward Bill',
-				region: 'Thailand',
-				score: 100500
-			};
-			let str = `<tr class="game-highScoreRow">
+        // this.GetUsersFromBack(3, 1);
+
+		const limit = 3;
+		const since = 1;
+
+        Transport.Get('/stop?limit=' + limit + '&since=' + since).then((response) => {
+            this.usersFromBack = response;
+        }).catch((response) => {
+            if (!response.json) {
+                console.log(response);
+                return;
+            }
+            response.json().then((json) => {
+                console.log(json);
+            });
+        });
+
+        // setTimeout(() => {
+        //     console.log(this.usersFromBack);
+        // }, 1000);
+
+        Handlebars.registerHelper('getRows', function () {
+            const user = {
+                name: 'Edward Bill',
+                region: 'Thailand',
+                score: 100500
+            };
+            let str = `<tr class="game-highScoreRow">
                          <th>Player</th>
                          <th>Region</th>
                          <th>Score</th>
                        </tr>`;
-			for (let i = 0; i < 3; i++) {
-				str += `<tr class="game-highScoreRow">
+            for (let i = 0; i < 3; i++) {
+                str += `<tr class="game-highScoreRow">
                          <td>${user.name}</td>
                          <td>${user.region}</td>
                          <td>${user.score}</td>
                        </tr>`;
-			}
-			return new Handlebars.SafeString(str);
-		});
-		this.template = Handlebars.compile(`<table class="game-highScrore">
+            }
+            return new Handlebars.SafeString(str);
+        });
+        this.template = Handlebars.compile(`<table class="game-highScrore">
                                                {{getRows}}
                                             </table>`);
-		this.template(data);
-		super.compile(data);
+        this.template(data);
+        super.compile(data);
 	}
+
+    // GetUsersFromBack(limit, since) {
+    //     return Transport.Get('/stop?limit=' + limit + '&since=' + since).then((response) => {
+    //         this.usersFromBack = response;
+    //         console.log(this.usersFromBack);
+    //     }).catch((response) => {
+    //         if (!response.json) {
+    //             console.log(response);
+    //             return;
+    //         }
+    //         response.json().then((json) => {
+    //             console.log(json);
+    //         });
+    //     });
+    // }
 }
