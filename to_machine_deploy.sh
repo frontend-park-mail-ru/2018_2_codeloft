32ea26c976c52bf3180903867c447c5581b2ab81
igor-dyrov <bazbazbaz.id@gmail.com>
2018-11-05 20:45:35 +0300

    .yml fix

--- to_machine_deploy.sh
+++ to_machine_deploy.sh
@@ -9,6 +9,6 @@ mv ./dist/* ~/archive
 tar -czf ~/package.tgz ~/archive
 
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o StrictHostKeychecking=no package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH 
+sshpass -e scp -o StrictHostKeychecking=no ~/package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH 
 sshpass -e ssh -o StrictHostKeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && ./deploy.sh ${BUILD}"
 
