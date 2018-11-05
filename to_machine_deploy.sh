865b1631d0d99a4e2fc36d487c3795f7d9cc7390
igor-dyrov <bazbazbaz.id@gmail.com>
2018-11-05 21:18:44 +0300

    .yml fix

--- to_machine_deploy.sh
+++ to_machine_deploy.sh
@@ -11,6 +11,6 @@ tar -czf ~/package.tgz ~/archive
 stat --printf="%s" ~/package.tgz
 
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o StrictHostKeychecking=no ~/package.tgz travis@$codeloft.ru:/home/travis/Deploy 
-sshpass -e ssh -o StrictHostKeychecking=no travis@$codeloft.ru "cd /home/travis/Deploy && ./deploy.sh"
+sshpass -e scp -o StrictHostKeychecking=no ~/package.tgz travis@codeloft.ru:/home/travis/Deploy 
+sshpass -e ssh -o StrictHostKeychecking=no travis@codeloft.ru "cd /home/travis/Deploy && ./deploy.sh"
 
