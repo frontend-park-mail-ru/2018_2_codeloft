19d1cc4fad32998eba84b0c1202c859280669d07
igor-dyrov <bazbazbaz.id@gmail.com>
2018-11-11 18:23:31 +0300

    yml and worker

--- to_machine_deploy.sh
+++ to_machine_deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-BUILD=${1:-testing}
-
 mv src/static dist
 
 tar -czf package.tgz dist
 
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o StrictHostKeychecking=no package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH 
-sshpass -e ssh -o StrictHostKeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && ./deploy.sh ${BUILD}"
+sshpass -e scp -o StrictHostKeychecking=no package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e ssh -o StrictHostKeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && ./$1.deploy.sh"
